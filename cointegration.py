4911041ffad6ba6c6b4d1af63ad853569864ec87
Ethan Mendes <eamendes88@gmail.com>
2020-03-19 23:41:11 -0400

    Finished cointegration

--- cointegration.py
+++ cointegration.py
@@ -1,27 +1,44 @@
 #https://machinelearningmastery.com/time-series-data-stationary-python/
 #Engle-Grange
 import pandas as pd
+import statsmodels.tsa.stattools as ts
+import seaborn
+
 
 import matplotlib.pyplot as plt
 data = []
 time = []
 stock_series = pd.read_csv('Stock Time.csv')
 
-'''
-for i in stock_series.columns:
-    #print(series[i])
-    if i == 1:
-        time.append(stock_series[i])
-    else:
-        data.append(stock_series[i])
-'''
+p_values = []
+coint_sec = []
 
-print(stock_series['Date'])
-x = stock_series['Date']
-y = stock_series['XOM']
-plt.plot(x,y, 'g-')
+tickers = ['XOM', 'RDS.A', 'CVX', 'TOT', 'BP', 'PTR', 'SNP', 'SLB', 'EPD', 'E', 'COP', 'EQNR', 'EOG', 'PBR', 'CEO', 'SU', 'OXY', 'HAL']
+for i in range(len(tickers)):
+    p_values.append([0] * len(tickers))
+    coint_sec.append([0] * len(tickers))
 
-axes = plt.gca()
+for i in range(0, len(tickers)):
+    for j in range(0, len(tickers)):
+        if (i < j) :
+            p_values[i][j] = (ts.coint(stock_series[tickers[i]], stock_series[tickers[j]]))[1]
+        else :
+            p_values[i][j] = 0.5
+
+threshold = 0.005
+
+for i in range(0, len(tickers)):
+    for j in range(0, len(tickers)):
+        if (i < j) :
+            if (p_values[i][j] < threshold):
+                coint_sec[i][j] = 1
+            else:
+                coint_sec[i][j] = 0
+        else :
+            p_values[i][j] = 0
+
+seaborn.heatmap(p_values, xticklabels=tickers,yticklabels=tickers, cmap='RdYlGn_r')
+seaborn.heatmap(coint_sec, xticklabels=tickers,yticklabels=tickers, cmap='RdYlGn_r')
 
 plt.show()
 
