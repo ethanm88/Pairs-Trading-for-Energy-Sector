834d22faa8aaf67782f26481c645e51256b65fdc
Ethan Mendes <eamendes88@gmail.com>
2020-03-23 02:07:14 -0400

    cleaned up testing method

--- trade.py
+++ trade.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import statsmodels.tsa.stattools as ts
 import seaborn
-import csv
+
 import numpy as np
 import cointegration
 import statistics
+
+# to do
+# 5 day moving average, more than 1 pair, 100,000
+
 
 import matplotlib.pyplot as plt
 
@@ -22,73 +26,79 @@ def z_score(val1, val2, lst):
     return (ratio - mean(lst)) / (stan_dev(lst))
 
 
-def bollinger_bands(ratio_lst, window_size, num_sd=1):
+def get_moving_average(ratio_lst, window_size):
     ratio = pd.DataFrame(ratio_lst)
     moving_average = ratio.rolling(window=window_size).mean()
     moving_average_lst = moving_average.values.tolist()
+    return moving_average, moving_average_lst
+
+
+def bollinger_bands(ratio_lst, window_size, num_sd=1):
+    ratio = pd.DataFrame(ratio_lst)
+    moving_average, moving_average_lst = get_moving_average(ratio_lst, window_size)
     moving_sd = ratio.rolling(window=window_size).std()
     moving_sd_lst = moving_sd.values.tolist()
     upper_band = (moving_average + (moving_sd * num_sd)).values.tolist()
     lower_band = (moving_average - (moving_sd * num_sd)).values.tolist()
+
     return moving_average_lst, moving_sd_lst, upper_band, lower_band
 
 
-def moving_z_score(ratio_lst, moving_average_lst, moving_sd_lst, index):
-    return (ratio_lst[index] - moving_average_lst[index][0]) / (moving_sd_lst[index][0])
+def moving_z_score(moving_average_short_lst, moving_average_lst, moving_sd_lst, index):
+    return (moving_average_short_lst[index][0] - moving_average_lst[index][0]) / (moving_sd_lst[index][0])
 
 
-def testing_moving(ratio, stock1, stock2, const):
-    netGains = 0
-    stock1_owned = 0
-    stock2_owned = 0
-
-    window_size = 20
+def trade_moving(ratio, stock1, stock2, index, window_size=20, short_window_size=5):
+    case = 0
+    multiplier = 1
     moving_average, moving_sd, upper_band, lower_band = bollinger_bands(ratio, window_size)
-
-    for i in range(window_size, len(ratio)):
-        if (ratio[i] > upper_band[i][0]):
-            multiplier = moving_z_score(ratio, moving_average, moving_sd, i)
-            stock1_owned -= min(stock1_owned, multiplier * const)
-            netGains += multiplier * stock1[i] * const
-
-            stock2_owned += multiplier * const
-            netGains -= multiplier * stock2[i] * const
-        elif (ratio[i] < lower_band[i][0]):
-            multiplier = -1 * moving_z_score(ratio, moving_average, moving_sd, i)
-            stock2_owned -= min(stock2_owned, multiplier * const)
-            netGains += multiplier * stock2[i] * const
-
-            stock1_owned += multiplier * const
-            netGains -= multiplier * stock1[i] * const
-
-    netGains += stock1_owned * stock1[len(ratio) - 1] + stock2_owned * stock2[len(ratio) - 1]
-
-    print(netGains)
-    print(stock1_owned)
-    print(stock2_owned)
+    moving_average_short, moving_average_short_lst = get_moving_average(ratio, short_window_size)
+    if (moving_average_short_lst[index][0] < lower_band[index][0]):
+        case = 2
+        multiplier = -1 * moving_z_score(moving_average_short_lst, moving_average, moving_sd, index)
+    elif (moving_average_short_lst[index][0] > upper_band[index][0]):
+        case = 1
+        multiplier = moving_z_score(moving_average_short_lst, moving_average, moving_sd, index)
+    return case, multiplier
 
 
-def testing_simple(ratio, stock1, stock2, const):
+def trade_simple(ratio, stock1, stock2, index):
+    cur_score = z_score(stock1[index], stock2[index], ratio)
+    case = 0
+    if (cur_score > 1):
+        case = 1
+    if (cur_score < -1):
+        case = 2
+    cur_score = max(cur_score, -1 * cur_score)
+    return case, cur_score
+
+
+def testing(ratio, stock1, stock2, model, const):
     netGains = 0
     stock1_owned = 0
     stock2_owned = 0
+    case = 0
+    cur_score = 0
 
     for i in range(0, len(ratio)):
-        cur_score = z_score(stock1[i], stock2[i], ratio)
-        if (cur_score > 1):
+        if (model == 1):  # simple
+            case, cur_score = trade_simple(ratio, stock1, stock2, i)
+        else:
+            case, cur_score = trade_moving(ratio, stock1, stock2, i)
+        if (case == 1):
             stock1_owned -= min(stock1_owned, cur_score * const)
             netGains += cur_score * stock1[i] * const
 
             stock2_owned += cur_score * const
             netGains -= cur_score * stock2[i] * const
-        elif (cur_score < -1):
-            stock2_owned -= min(stock2_owned, -1 * cur_score * const)
-            netGains += -1 * cur_score * stock2[i] * const
+        elif (case == 2):
+            stock2_owned -= min(stock2_owned, cur_score * const)
+            netGains += cur_score * stock2[i] * const
 
-            stock1_owned += -1 * cur_score * const
-            netGains -= -1 * cur_score * stock1[i] * const
+            stock1_owned += cur_score * const
+            netGains -= cur_score * stock1[i] * const
 
-    netGains += stock1_owned * stock1[len(ratio) - 1] + stock2_owned * stock2[len(ratio) - 1]
+    # netGains += stock1_owned * stock1[len(ratio) - 1] + stock2_owned * stock2[len(ratio) - 1]
 
     print(netGains)
     print(stock1_owned)
@@ -105,6 +115,10 @@ def main():
     stock2 = stock_series[pairs[1][1]].tolist()
 
     ratio = np.divide(stock_series[pairs[1][0]].tolist(), stock_series[pairs[1][1]].tolist())
+
+    # testing_moving(ratio, stock1, stock2, 1)
+    testing(ratio, stock1, stock2, 1, 1)
+    testing(ratio, stock1, stock2, 2, 1)
 
 
 main()
